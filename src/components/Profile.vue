8dfab02e194682ae6e6a5dac57992eab55a80dac
Andreas Teder <andreasteder@gmail.com>
2019-11-04 19:48:18 +0200

    Halfway there

---
+++ src/components/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="profile-container" class="tab active">
+        <div id="profile">
+            <div class="avatar">
+                <img src="../../src/assets/me.png" id="picture" alt="My picture">
+            </div>
+            <div class="info">
+                <ul>
+                    <li id="name">{{this.user.firstname}} {{this.user.lastname}}</li>
+                    <li id="birthdate">{{this.user.birthdate}}</li>
+                    <li id="faculty">{{this.user.faculty}}</li>
+                </ul>
+            </div>
+            <div id="gpa">
+                <strong>2.75</strong>
+            </div>
+            <div class="clear-fix"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Profile",
+        props: {
+            user: Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
